993574141f0adcf79dcca22738713d1118a3eb1f
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-23 17:31:50 -0400

    controllers created for projects, users, sessions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,22 @@
 Rails.application.routes.draw do
+
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  resources :projects
+  resources :users, except: :index
+  resources :sessions, only: [:new, :create, :destroy]
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'projects#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
