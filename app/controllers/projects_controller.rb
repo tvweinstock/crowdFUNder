01cc84e6bc9493924cd70f3f2fc15a299e20fe0c
Moufid Larbi <moufid4@gmail.com>
2014-09-24 18:11:19 -0400

    updated projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,49 @@
 class ProjectsController < ApplicationController
+	def index
+    	@projects = Project.all
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	def new
+		@project = Project.new		
+	end
+
+	def create
+	 @project = Project.new(project_params)
+		 if @project.save
+		 	redirect_to projects_url
+		 else
+		 	render :new
+		 end 
+	end
+	
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])
+
+		if @project.update_attributes(project_params)
+			redirect_to "/projects/#{@project.id}"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		Project.find(params[:id]).destroy
+		redirect_to projects_url, :flash => { :success => "Project destroyed." }
+	end
+
+
+	private
+	def project_params
+		params.require(:project).permit(:name, :description, :goal, :start_date, :end_date)
+		
+	end	
+
 end
