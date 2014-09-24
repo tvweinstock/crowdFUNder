f89a0cf34f097befb61ced6d43740dc73fae848e
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-24 13:54:49 -0400

    get login and get logout routes applied

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-
+  get 'login' => 'sessions#new'
+  get 'logout' => 'sessions#destroy'
   resources :projects
   resources :users, except: :index
   resources :sessions, only: [:new, :create, :destroy]
