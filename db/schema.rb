fdb1e63a62d5a85164b776829e67236ddc11f74b
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-24 14:22:01 -0400

    user table dropped sorcery gem to be added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140923204504) do
+ActiveRecord::Schema.define(version: 20140924181516) do
 
   create_table "donations", force: true do |t|
     t.integer  "amount"
@@ -36,14 +36,6 @@ ActiveRecord::Schema.define(version: 20140923204504) do
     t.string   "description"
     t.integer  "amount"
     t.integer  "project_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
