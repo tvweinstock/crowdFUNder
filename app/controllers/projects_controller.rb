8e299428601015f4e9cd42ffe36276aef81b4020
Tobi Weinstock <tv.weinstock@gmail.com>
2014-09-25 18:41:11 -0400

    user now able to view their projects on show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -13,7 +13,9 @@ class ProjectsController < ApplicationController
 
 	def create
 	 @project = Project.new(project_params)
+	 @project.user_id = current_user.id
 		 if @project.save
+
 		 	redirect_to projects_url
 		 else
 		 	render :new
