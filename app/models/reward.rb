923349765a38b05b0f5be50a4cbfcfb0a69c9a11
Moufid Larbi <moufid4@gmail.com>
2014-09-25 18:03:31 -0400

    Reward function in projects/show.html.erb file working

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,4 +1,12 @@
 class Reward < ActiveRecord::Base
   belongs_to :project
   has_many :donations
+  has_many :rewards, through: :donations
+
+  def donation
+  	@donations.rewards.each do |reward|
+
+  	end
+  end
+  
 end
